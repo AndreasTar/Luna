685c1680e12d37f1bef65cfaf2b9ad0f345b4a69
Andy Pandy <andytgamedev@gmail.com>
2024-07-02 20:25:04 +0300

    theres some compilation error on egui

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
 #![allow(unused, dead_code)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+//#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 
 use eframe::egui;
 
 use math::convert_number_base;
+use ui::Luna;
 
 mod math;
 mod ui;
 
 
 fn main() -> eframe::Result<()> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    // env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
             .with_min_inner_size([300.0, 220.0]),
-            // .with_icon(
-            //     // NOTE: Adding an icon is optional
+            // optional
+            // .with_icon( 
             //     eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
             //         .expect("Failed to load icon"),
             // ),
@@ -27,7 +28,7 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "Luna",
         native_options,
-        Box::new(|cc| Box::new(Luna::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(Luna::default())),
     )
 }
 
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,18 +1,25 @@
 
-struct Luna {
-    pages: Vec<ToolPages>,
+pub struct Luna {
+    //pages: Vec<ToolPages>,
 }
 
 impl Default for Luna {
     fn default() -> Self {
         return Self { 
-            pages: () 
+            //pages: () 
         }
     }
 }
 
 impl eframe::App for Luna {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        
+        egui::SidePanel::left("tools_sidepanel_window")
+            .resizable(false)
+            .default_width(150.0)
+            .show(ctx, |ui| {
+                ui.vertical_centered_justified(|ui|{
+                    ui.heading("Tools");
+                });
+            });
     }
 }
