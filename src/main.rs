34ca41fdd1b877c8840fc79bd2bc04d858a25a43
Andy Pandy <andytgamedev@gmail.com>
2024-07-02 17:59:31 +0300

    started on conversion to egui and eframe

--- src/main.rs
+++ src/main.rs
@@ -4,65 +4,17 @@ use core::num;
 use std::vec;
 
 use math::convert_number_base;
-use slint::{ComponentHandle, Model, ModelRc, SharedString, VecModel, WindowPosition};
 
 mod math;
 
-slint::include_modules!();
 
-fn main() -> Result<(), slint::PlatformError> {
-    let ui = MyWindow::new()?;
-
-    //ui.window().set_position(WindowPosition::Logical(slint::LogicalPosition { x: (1000.0), y: (600.0) }));
-
-    ui.global::<ConversionCallback>().on_request_convert_number({
-        let ui_handle = ui.as_weak();
-
-        // move |in_from, in_to, in_num:SharedString| {
-        //     if in_num.is_empty() {
-        //         return SharedString::from("");
-        //     }
-
-        //     let ui = ui_handle.unwrap();
-        //     let number = convert_number_base(in_from as u32, in_to as u32, in_num.to_string());
-
-        //     return number.into();
-        // }
-
-        move |in_numbers, in_bases, in_index, in_num| {
-
-            let mut out_numbers: Vec<SharedString> = vec![];
-            for (i, base) in in_bases.iter().enumerate() {
-                //if i == in_index as usize { continue; }
-
-                out_numbers.insert(
-                    i, 
-                    { if (i == in_index as usize) { 
-                                in_num.clone()
-                            } else { 
-                                convert_number_base(
-                                    in_bases.iter().nth(in_index as usize).unwrap() as u32,
-                                    base as u32,
-                                    in_num.clone().to_string()
-                                ).into()
-                        }
-                    }
-                );
-            }
-
-            return out_numbers.as_slice().into();
-        }
-
-    });
-
-
-    ui.run()
+fn main(){
+    return;
 }
 
 
-
-
 /*\*
+ * THIS NEEDS UPDATING FOR EGUI AND EFRAME
  * the layout i have thought of:
  * 
  * UI will have many buttons and pages etc
