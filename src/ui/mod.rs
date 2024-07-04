df1908be5ac9d85f419a2e9d6f321085f0885f01
Andy Pandy <andytgamedev@gmail.com>
2024-07-04 17:25:57 +0300

    added some logic for sidebar

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,12 +1,16 @@
 
+mod tool_pages;
+
+use tool_pages::ToolPages;
+
 pub struct Luna {
-    //pages: Vec<ToolPages>,
+    pages: Vec<ToolPages>,
 }
 
 impl Default for Luna {
     fn default() -> Self {
         return Self { 
-            //pages: () 
+            pages: vec![ToolPages::new("temp", "test").set_enabled(true)]
         }
     }
 }
@@ -20,6 +24,27 @@ impl eframe::App for Luna {
                 ui.vertical_centered_justified(|ui|{
                     ui.heading("Tools");
                 });
+                egui::Grid::new("tools_sidepanel_content")
+                    .num_columns(2)
+                    .striped(true)
+                    .spacing([50.0, 5.0])
+                    .show(ui, |ui| {
+                        self.show_sidebar_pages(ui);
+                    });
             });
+    }
+}
+
+impl Luna {
+    fn show_sidebar_pages(&self, ui: &mut egui::Ui){
+        for page in &self.pages{
+            ui.label(&page.sidebar_name);
+            let mut temp = page.enabled;
+            // ui.checkbox(&mut temp, "Enabled?").without_text(true);
+            // ui.add(egui::Checkbox::without_text(&mut temp));
+            ui.add_enabled(temp, egui::Checkbox::without_text(&mut temp));
+            ui.end_row()
+
+        }
     }
 }
---
+++ src/ui/tool_pages.rs
@@ -0,0 +1,49 @@
+use std::default;
+
+
+
+pub struct ToolPages{
+    pub enabled: bool,
+    pub sidebar_name: String,
+    pub title_name: String
+}
+
+impl Default for ToolPages {
+    fn default() -> Self {
+        return Self{
+            enabled: false,
+            sidebar_name: String::from("Default sidebar name"),
+            title_name: String::from("Default title name")
+        };
+    }
+}
+
+impl ToolPages{
+    pub fn new(sidebar: &str, title: &str) -> Self {
+        return Self{
+            sidebar_name: sidebar.to_string(),
+            title_name: title.to_string(),
+            ..Default::default()
+        };
+    }
+
+    pub fn add_to_sidebar(&self) -> &Self{
+        todo!()
+    }
+
+    pub fn remove_from_sidebar(&self) -> &Self{
+        todo!()
+    }
+
+    pub fn set_enabled(self, enable: bool) -> Self{
+        return Self {
+            enabled: enable,
+            ..self
+        };
+    }
+
+    pub fn set_enabled_as_ref(&mut self, enable: bool) -> &Self {
+        self.enabled = enable;
+        return self;
+    }
+}
